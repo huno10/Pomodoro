ae3d298b017a1df47a685a855458641d66d26273
Unan <hunanhopar@mail.ru>
2024-01-26 19:25:47 +0400

    Обновление 25.01

--- src/components/menu/timer/Timer.jsx
+++ src/components/menu/timer/Timer.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import styles from './Timer.module.css'
+import intervalSvg from '../../../img/addInterval.svg'
 
 const pomodor = 'Помидор 1'
+const index = '2'
+const name = 'Сверстать сайт'
 
 export const Timer = () => {
   return (
@@ -9,6 +12,23 @@ export const Timer = () => {
       <div className={styles.toDo_header}>
         <h3 className={styles.title}>Сверстать сайт</h3>
         <p className={styles.toDo_name}>{pomodor}</p>
+      </div>
+      <div className={styles.timer_block}>
+        <div className={styles.timer}>
+          <p className={styles.interval}>25:00</p>
+          <button className={styles.add_interval}>
+            <img src={intervalSvg} alt="" />
+          </button>
+        </div>
+        <div>
+          <p className={styles.toDo_info}>
+            <span className={styles.toDo_span}>{`Задача ${index} - `}</span>
+            {name} </p>
+        </div>
+        <div className={styles.btn_wrapper}>
+          <button className={styles.btn_start}>Старт</button>
+          <button className={styles.btn_stop}>Стоп</button>
+        </div>
       </div>
     </div>
   )
--- src/components/menu/toDoBlock/toDo/toDoItem/ToDoItem.jsx
+++ src/components/menu/toDoBlock/toDo/toDoItem/ToDoItem.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import styles from './ToDoItem.module.css'
 import btnItem from '../../../../../img/btn-item.svg'
+import incSvg from '../../../../../img/inc.svg'
+import dicSvg from '../../../../../img/dic.svg'
+import redSvg from '../../../../../img/red.svg'
+import deleteSvg from '../../../../../img/delete.svg'
 
 export const ToDoItem = ({ task, index }) => {
+    const ref = useRef(null);
+    const [dropDownOpen, setDropDownOpen] = useState(false)
+
+    const toggleDropDown = () => {
+        setDropDownOpen((prevState) => !prevState);
+    };
+
+    const handleClickOutside = useCallback((event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+            const isButtonClick = event.target.classList.contains(styles.btn) ||
+                event.target.closest(`.${styles.btn_dropdown}`) ||
+                event.target.closest(`.${styles.dropdown_list}`);
+
+            if (!isButtonClick) {
+                setDropDownOpen(false);
+            }
+        }
+    }, [dropDownOpen, ref]);
+
+    useEffect(() => {
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [handleClickOutside]);
 
     return (
         <li className={styles.item}>
@@ -10,9 +39,34 @@ export const ToDoItem = ({ task, index }) => {
                 <span className={styles.index}>{index + 1}</span>
                 <h3 className={styles.title}>{task}</h3>
             </div>
-            <button className={styles.btn}>
-                <img src={btnItem} />
-            </button>
+            <div ref={ref} className={styles.dropDown_wrapper}>
+                <button className={styles.btn} type='button' onClick={toggleDropDown} >
+                    <img src={btnItem} />
+                </button>
+                {dropDownOpen && (
+                    <div className={styles.btn_dropdown} >
+                        <div className={styles.dropdown_list} >
+                            <button type='button' className={styles.dropdown_item}>
+                                <img className={styles.dropdown_img} src={incSvg} alt="" />
+                                <span className={styles.dropdown_span}>Увеличить</span>
+                            </button>
+                            <button type='button' className={styles.dropdown_item}>
+                                <img className={styles.dropdown_img} src={dicSvg} alt="" />
+                                <span className={styles.dropdown_span}>Уменшить</span>
+                            </button>
+                            <button type='button' className={styles.dropdown_item}>
+                                <img className={styles.dropdown_img} src={redSvg} alt="" />
+                                <span className={styles.dropdown_span}>Редактировать</span>
+                            </button>
+                            <button type='button' className={styles.dropdown_item}>
+                                <img className={styles.dropdown_img} src={deleteSvg} alt="" />
+                                <span className={styles.dropdown_span}>Удалить</span>
+                            </button>
+                        </div>
+                    </div>
+                )}
+
+            </div>
         </li>
     )
 }
